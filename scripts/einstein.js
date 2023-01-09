cac37952749cf4c6e7c9b232dec04a89de99653f
Aleksandr-Viktorovich <teanvkz@gmail.com>
2023-01-09 21:40:13 +0700

    04

---
+++ scripts/einstein.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myWeight = 8;
+const speedLight = 3e8;
+const bodyEnergi = (myWeight * speedLight) ** 2;
+
+console.log(bodyEnergi);
---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const product = 'телевизор';
+const remainder = 10;
+const category = 'техника';
+const price = 100;
+
+
+{
+  const product = prompt ('Наименование товара?', 'телевизор');
+  let remainder = +prompt ('Количество товара?',10);
+  const category = prompt ('Категория товара?', 'техника');
+  let price = +prompt ('Цена товара?', 100);
+
+
+
+  console.log(typeof (product));
+  console.log(typeof (remainder));
+  console.log(typeof (category));
+  console.log(typeof (price));
+
+  console.log(`у меня ${remainder} единиц "${product}" на сумму ${remainder * price} православных=))`);
+}
+
+
